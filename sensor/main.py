79c328b3c2c9ed71ff0e45248b428e10e15a4937
idur69 <shaikidurbasha469@gmail.com>
2023-04-07 13:02:02 +0530

    final code commit

--- sensor/main.py
+++
@@ -1,7 +0,0 @@
-from sensor.configuration.mongo_db_connection import MongoClient
-
-
-if __name__ == "__main__":
-
-    mongodb_client = MongoClient()
-    print("Mongo DB Connections :", mongodb_client.database.list_collection_names())
--- setup.py
+++ setup.py
@@ -1,25 +1,23 @@
-from setuptools import find_packages, setup 
+from setuptools import find_packages,setup
 from typing import List
 
-def get_requirments()->List[str]:
+def get_requirements()->List[str]:
     """
     This function will return list of requirements
-
-    Returns:
-        List[str]: _description_
     """
-    requirements_list : List[str] = []
+    requirement_list:List[str] = []
 
     """
     Write a code to read requirements.txt file and append each requirements in requirement_list variable.
     """
-    return requirements_list
+    return requirement_list
 
 setup(
     name="sensor",
     version="0.0.1",
-    author="idur",
-    author_email="shaikidurbasha469@gmail.com",
-    package=find_packages(),
-    install_requires=get_requirments(),#"pymongo==4.2.0",
+    author="ineuron",
+    author_email="avnish@ineuron.ai",
+    packages = find_packages(),
+    install_requires=get_requirements(),#["pymongo==4.2.0"],
 )
+
